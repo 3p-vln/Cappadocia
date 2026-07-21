9f46fe1e1821d32a98ad27896f8fe55af9bdd1f1
3p-vln <vldknovak@gmail.com>
2026-07-21 20:27:02 +0300

    FORM FOR CONTACT validate

--- main.js
+++ main.js
@@ -22,3 +22,45 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-prev',
 	},
 });
+
+const validate = new window.JustValidate('.form__contact');
+
+validate
+	.addField('.form__name', [
+		{
+			rule: 'required',
+			errorMessage: `Введіть ваше ім'я`,
+		},
+		{
+			rule: 'minLength',
+			value: 2,
+			errorMessage: `Довжина ім'я повинна бути більше 1 літери`,
+		},
+		{
+			rule: 'maxLength',
+			value: 50,
+			errorMessage: `Довжина ім'я повинна бути менше`,
+		},
+		{
+			rule: 'customRegexp',
+			value: /^[\p{L}’'\-]+$/u,
+			errorMessage: `Допускаються тільки літери, апостроф та дефіс`,
+		},
+	])
+	.addField('.form__phone', [
+		{
+			rule: 'required',
+			errorMessage: 'Введіть номер телефону',
+		},
+		{
+			validator: (value) => {
+				const phone = value.replace(/\D/g, '');
+				
+				return (
+					/^380\d{9}$/.test(phone) ||
+					/^0\d{9}$/.test(phone)
+				);
+			},
+			errorMessage: 'Введіть коректний номер телефону',
+		},
+	]);
