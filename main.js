42f944f592a243f9f6d204594c92389fee5982a8
3p-vln <vldknovak@gmail.com>
2026-07-21 13:28:42 +0300

    header scroll animation

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs'
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 20) {
+		header.classList.add('scrolled');
+	} else {
+		header.classList.remove('scrolled');
+	}
+});
+
 const swiper = new Swiper('.swiper', {
 	loop: true,
 	
